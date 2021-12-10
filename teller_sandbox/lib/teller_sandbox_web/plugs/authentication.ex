e76f1975e2f16b122e361cbc433298f63cb40fbc
joaowozniak <joao.wozniak@gmail.com>
2021-12-10 19:31:39 +0000

    added auth w/ token

--- teller_sandbox/lib/teller_sandbox_web/plugs/authentication.ex
+++ teller_sandbox/lib/teller_sandbox_web/plugs/authentication.ex
@@ -1,22 +1,46 @@
 defmodule TellerSandboxWeb.Plugs.Authentication do
   import Plug.Conn
 
-  def init(options), do: options
+  def init(opts), do: opts
 
   def call(conn, _opts) do
-    with {user, _} <- Plug.BasicAuth.parse_basic_auth(conn),
-      {:ok, _} <- verify_user(user) do
-        IO.puts(:ok)
-      #%User{} = user <- MyApp.Accounts.find_by_username_and_password(user, pass) do
-      assign(conn, :user, user)
+    with {user, pass} <- Plug.BasicAuth.parse_basic_auth(conn),
+        {:valid, accounts} <- verify_user(user, pass) do
+
+          token = token_from_user(user)
+
+      assign(conn, :token, token)
+      assign(conn, :accounts, accounts)
     else
-      _ -> conn |> Plug.BasicAuth.request_basic_auth() |> halt()
+      _ -> Plug.BasicAuth.request_basic_auth(conn) |> halt()
     end
   end
 
-  def verify_user(user) do
-    IO.puts(user)
-    {:ok, true}
+  defp verify_user(user, password) do
+    cond do
+
+      (String.starts_with?(user, "user_") &&
+      String.length(user) == 10 &&
+      Regex.match?(~r{\A\d*\z}, String.slice(user, 5, 9)) &&
+      String.length(password) == 0) ->
+
+        {:valid, "one"}
+
+      (String.starts_with?(user, "user_multiple") &&
+      String.length(user) == 19 &&
+      Regex.match?(~r{\A\d*\z}, String.slice(user, 14, 18)) &&
+      String.length(password) == 0) ->
+
+        {:valid, "multiple"}
+
+      true -> {:not_valid, false}
+
+    end
+  end
+
+  defp token_from_user(user) do
+    encoded = Base.encode64(user)
+    token = "test_" <> encoded
   end
 
 end
